01e7787e2def7e1e20532af22b0ec9146de4c081
lephiha <phihasky@gmail.com>
2025-06-06 10:01:48 +0700

    UI dasboard

--- app/src/main/java/com/lephiha/coffee_shop_app/SplashActivity.kt
+++ app/src/main/java/com/lephiha/coffee_shop_app/SplashActivity.kt
@@ -1,20 +1,27 @@
 package com.lephiha.coffee_shop_app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.lephiha.coffee_shop_app.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            // Navigate to MainActivity
+            startActivity(Intent(this, MainActivity::class.java))
+            finish() // Close SplashActivity
         }
+
     }
 }
